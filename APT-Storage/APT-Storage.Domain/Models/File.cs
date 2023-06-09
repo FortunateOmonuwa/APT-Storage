f4496699f64ec7dbfd69571ef5c808c77295ea62
Fortunate Omonuwa <omonuwafortunate@gmail.com>
2023-06-09 16:08:54 +0100

    Added Properties to Model classes

--- APT-Storage/APT-Storage.Domain/Models/File.cs
+++ APT-Storage/APT-Storage.Domain/Models/File.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +11,20 @@ namespace APT_Storage.Domain.Models
 {
     public class File
     {
+        [Key]
+        public int Id { get; set; }
 
+        [MaxLength(20)]
+        [DisplayName("Enter File Name")]
+        public string FileName { get; set; }
+
+        [Range(0, 5L * 1024 * 1024, ErrorMessage = "file size too large")]
+        public long FileSize { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime UploadDate { get; set; } = DateTime.Now;
+
+        [ForeignKey(nameof(User))]
+        public int OwnerId { get; set; }
     }
 }
--- APT-Storage/APT-Storage.Domain/Models/Folder.cs
+++ APT-Storage/APT-Storage.Domain/Models/Folder.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,23 @@ namespace APT_Storage.Domain.Models
 {
     public class Folder
     {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(20)]
+        [DisplayName("Enter Folder Name")]
+        public string Name { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public int OwnerId { get; set; }
+        //public int? ParentFolderId { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        [DataType(DataType.DateTime)]
+        public DateTime? UpdatedAt { get; set; }
+
+        public ICollection<Folder>? Subfolders { get; set; }
+        public ICollection<File>? Files { get; set; }
     }
 }
